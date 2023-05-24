ad4422ae2d841aa3e962df048661f6b6033d517e
Mohammed Haroon <haroon_calculus@hotmail.com>
2023-05-24 21:01:02 +0530

    Added code for Supplier managment

---
+++ GroceryStore.Api/Controllers/SupplierManagementController.cs
@@ -0,0 +1,62 @@
+﻿using GroceryStore.Api.DataAccessLayer;
+using GroceryStore.Api.DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+
+namespace GroceryStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupplierManagementController : ControllerBase
+    {
+        private readonly DataContext _db;
+        private readonly ILogger<UserManagementController> _logger;
+        public SupplierManagementController(ILogger<UserManagementController> logger)
+        {
+            _logger = logger;
+            _db = new DataContext();
+
+        }
+        [HttpPost]
+        [Route("~/api/AddSupplier")]
+        public void AddSupplier(string supplierjson)
+        {
+
+            //OrderResponse oresponse = null;
+            try
+            {
+                SupplierManangement supplier = JsonConvert.DeserializeObject<SupplierManangement>(supplierjson);
+                if (!string.IsNullOrEmpty(supplier.Name) &&
+                    !string.IsNullOrEmpty(supplier.Phone) &&
+                    !string.IsNullOrEmpty(supplier.Bank)
+                    )
+                {
+                    GSDatabaseManager gsMgmt = new GSDatabaseManager(_db);
+                    gsMgmt.AddorUpdateSupplier(supplier);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            //return oresponse;
+        }
+        [HttpPost]
+        [Route("~/api/deletesupplier")]
+        public void DeleteSupplier(int id)
+        {
+            try
+            {
+
+
+                GSDatabaseManager gsMgmt = new GSDatabaseManager(_db);
+                gsMgmt.DeleteSupplierbyId(id);
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
